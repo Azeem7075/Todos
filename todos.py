45dfc068f8def0a653a75579e4c371d8ec88d3a5
Azeem <azeem0074ushaik@gmail.com>
2026-05-04 15:15:22 +0530

    added the pydantic

--- todos.py
+++ todos.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, Depends
-from database import Base, SessionLocal , engine 
+from database import Base,  engine 
 from modules import Todos , dependency
 from sqlalchemy.orm import Session
 from schemas import TodoSchema
@@ -9,35 +9,39 @@ app = FastAPI()
 
 Base.metadata.create_all(bind = engine)
 
-@app.post("/")
+@app.post("/", status_code= 201)
 def insertit(data:TodoSchema, db:Session = Depends(dependency)):
     test = Todos(text = data.text)
     db.add(test)
     db.commit()
+    return  test
 
 
-@app.get("/")
+@app.get("/", status_code= 200)
 def see_all(db:Session = Depends(dependency)):
     data = db.query(Todos).all()
     return data
 
 
-@app.get("/get_id/{id}")
+@app.get("/get_id/{id}", status_code= 200)
 def get_by_id(id:int, db:Session = Depends(dependency)):
     data = db.query(Todos).filter(Todos.id == id).first()
     return data
+    
 
 
-@app.put("/{id}")
+@app.put("/{id}", status_code= 200)
 def update(id:int, data:TodoSchema,db:Session = Depends(dependency)):
     store = db.query(Todos).filter(Todos.id== id).first()
     store.text = data.text
     db.commit()
+    return "todo had been updated"
 
-@app.delete("/{id}")
+@app.delete("/{id}", status_code=204)
 def delete(id:int, db: Session = Depends(dependency)):
     store = db.query(Todos).filter(Todos.id == id).first()
     db.delete(store)
     db.commit()
+   
 
  
